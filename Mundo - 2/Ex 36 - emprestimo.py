0f1178c7844e5f3670be07ee3ee16523abcebfca
Lucas Santos <lucas.parkl@outlook.com>
2020-11-24 15:49:26 -0300

    Create Ex 36 - emprestimo.py

---
+++ Mundo - 2/Ex 36 - emprestimo.py
@@ -0,0 +1,10 @@
+valorcasa = float(input('Digite o valor da casa:\n'))
+salario =float(input('Digite o salário:\n')) 
+anos = int(input('Em quantos anos será pago\n'))
+
+prest= valorcasa/(anos*12)
+
+if prest>(salario+(salario*0.30)):
+    print(f"Emprestimo negado, valor da prestaçã excede 30% a mais do salário\nsalario: {salario}\nprestação mensal: {prest}")
+else:
+    print(f'Emprestimo aprovado\nPrestação mensal: {prest}')
