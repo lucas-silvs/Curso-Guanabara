3f55775f7db8da56431bcf4c8e83af7764c75c02
Lucas Santos <lucas.parkl@outlook.com>
2020-11-24 15:00:28 -0300

    cores

--- Mundo - 1/Ex 35 - Triangulo.py
+++ Mundo - 1/Ex 35 - Triangulo.py
@@ -3,6 +3,6 @@ b = float(input('Dige o tamanho do segmento de reta:\n'))
 c = float(input('Dige o tamanho do segmento de reta:\n'))
 
 if( ( a>(abs (b-c) ) and a< b+c )  or ( b>(abs (a-c) ) and b< a+c ) or ( c>(abs (a-b) ) and c< a+b )):
-    print("As retas forma um triângulo")
+    print("\033[0;34;40m As retas forma um triângulo\033[m")
 else:
-    print('As retas não forma um triângulo')
+    print('\033[4;33;40m As retas não forma um triângulo\033[m')
