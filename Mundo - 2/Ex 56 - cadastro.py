b9f7df1967b2bcb0616f680d5264d52d0a8ca3ec
Lucas Santos <lucas.parkl@outlook.com>
2020-11-24 23:33:30 -0300

    Update Ex 56 - cadastro.py

--- Mundo - 2/Ex 56 - cadastro.py
+++ Mundo - 2/Ex 56 - cadastro.py
@@ -1,12 +1,14 @@
 hmais=''
 idadeant=0
 qtdemulher=0
+som=0
 
 for i in range(4):
     
     nome = input('Digite o nome:\n')
     s = input('''Digite o sexo: F- Feminino; M - Masculino\n''')
     idade = int(input('Digite a idade da pessoa:\n'))
+    som=som+idade
 
     if (s=='M' and hmais==''):
         hmais = nome
@@ -19,6 +21,7 @@ for i in range(4):
     elif s=='F' and idade <20:
         qtdemulher+=1
 
-print(f'''Nome do homem mais velho: {hmais}
+print(f'''Media da idade de todos: {som/4}
+Nome do homem mais velho: {hmais}
 Qtde de mulheres menores de 20 anos: {qtdemulher}''')
 
